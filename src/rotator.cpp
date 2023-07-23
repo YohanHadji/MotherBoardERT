6ed9c4aadd845d5abd7bc20f253b15668ceda447
YohanHadji <hadji.yohan@gmail.com>
2023-07-23 18:25:39 +0200

    Save good working version

--- src/rotator.cpp
+++ src/rotator.cpp
@@ -117,7 +117,6 @@ rotatorCommand rotClass::computeCommand() {
 };
 
 TRACKING_MODE rotClass::computeMode() {
-    binoc.status.isInView = true;
     if (binoc.status.isInView and binoc.status.isCalibrated) {
         return TRACKING_FAST;
     } 
